a562d1ecdce976fd1b636a175c481774307feb1a
timster2938 <ruggt001@icloud.com>
2018-02-05 21:23:54 -0500

    add inquirer and prompting

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // use letter.js for checking and displaying letters of the word
 var Word = require("./word.js");
+var inquirer = require("inquirer");
 var guessedLetter = "";
 var guessedLetters = [];
 var availableCharacters = "abcdefghijklmnopqrstuvwxyz";
@@ -21,54 +22,59 @@ console.log("  that letter will be displayed in all places.\n");
 console.log("  You win when the entire word is displayed!\n");
 console.log("  If the letter guessed is not anywhere in the");
 console.log("  word then it is an incorrect guess. You can");
-console.log("  make ten incorrect guesses before you lose.\n");
+console.log("  make ten incorrect guesses before you lose.");
 
-// function playGame() {
-    function playGame(guessedLetter) {
-// prompt user for input
-// inquire for guessedLetter
-// transform to lowercase
-// test for invalid and previous guesses
-if (testLetter(guessedLetter)) {
-    // evaluate valid letter against word
-    if (gameWord.guessLetter(guessedLetter)) {
-        console.log("\nC O R R E C T ! ! !\n");
-        console.log("Letter " + guessedLetter + " was found");
-        console.log(gameWord.displayWord());
-// console.log(gameWord.lettersDisplayed);
-// console.log(gameWord.word);
-// console.log(gameWord.word.length);
-        if (gameWord.lettersDisplayed == gameWord.word.length) {
-            console.log("Win game");
-        };
-    } else {
-        console.log("\nW R O N G ! ! !\n");
-        console.log("Letter " + guessedLetter + " was NOT found");
-        console.log(gameWord.displayWord());
-        gameCountdownGuesses--;
-        console.log("Remaining guesses: " + gameCountdownGuesses);
-        if (gameCountdownGuesses == 0) {
-            console.log("Lose game");
+playGame();
+
+function playGame() {
+console.log("\n");
+    // prompt user for input and inquire for guess
+    inquirer.prompt([
+        {
+        name: "userGuess",
+        message: "Guess a letter: "
         }
-    }
-} else {
-    console.log("Play Game and prompt user for new input");
-    playGame("i");
-    // playGame()
-};
-};
+    ]).then(function(answers) {
+        // transform to lowercase
+        guessedLetter = answers.userGuess.toLowerCase();
+        // test for invalid and previous guesses
+        if (testLetter(guessedLetter)) {
+                // evaluate valid letter against word
+                if (gameWord.guessLetter(guessedLetter)) {
+                        outputStatus("C O R R E C T", "was found");
+                        if (gameWord.lettersDisplayed == gameWord.word.length) {
+                            console.log("\n************* W I N N E R ! *************");
+                            return
+                        };
+                        playGame();
+                } else {
+                        outputStatus("  W R O N G  ", "was NOT found");
+                        gameCountdownGuesses--;
+                        console.log("Remaining guesses: " + gameCountdownGuesses);
+                        if (gameCountdownGuesses == 0) {
+                            console.log("\n*************   L O S E R   *************");
+                            return
+                        };
+                        playGame();
+                };
+        } else {
+            playGame();
+        };
+    }); // end of iquirer
+}; // end of playGame
 
+// send to check for invalid character or previously guessed characters and if ok push into guessedLetters array
 function testLetter(guessedLetter) {
-if (wasLetterGuessed(guessedLetter)) {
-    console.log("\nYou've already guessed letter " + guessedLetter + ". Try again.");
-    return false
-        } else if (isValidLetter(guessedLetter) === false) {
-            console.log("\nCharacter " + guessedLetter + " is not a valid a-z letter. Try again.");
-            return false
+    if (wasLetterGuessed(guessedLetter)) {
+        console.log("\nYou've already guessed letter " + guessedLetter + ". Try again.");
+        return false
+            } else if (isValidLetter(guessedLetter) === false) {
+                console.log("\nCharacter " + guessedLetter + " is not a single valid a-z letter. Try again.");
+                return false
     } else {
-    // letter was not guessed before and is valid
-    guessedLetters.push(guessedLetter);
-    return true
+        // letter was not guessed before and is valid
+        guessedLetters.push(guessedLetter);
+        return true
     };
 };
 
@@ -78,8 +84,8 @@ function isValidLetter(guessedLetter) {
     for (var i=0; i < availableCharacters.length; i++) {
         if (guessedLetter == availableCharacters[i]) {
             letterFound = true; // set to true (false does not overwrite)
-        }
-    }
+        };
+    };
     return letterFound;
 };
 
@@ -89,47 +95,13 @@ function wasLetterGuessed(guessedLetter) {
     for (var i=0; i < guessedLetters.length; i++) {
         if (guessedLetter == guessedLetters[i]) {
             letterFound = true; // set to true (false does not overwrite)
-        }
-    }
+        };
+    };
     return letterFound;
 };
 
-
-// TEST
-playGame("s");
-playGame("e");
-playGame("t");
-playGame("a");
-playGame("g");
-playGame("m");
-playGame("n");
-playGame("u");
-playGame("c");
-playGame("b");
-playGame("p");
-playGame("q");
-playGame("w");
-playGame("x");
-playGame("z");
-playGame("l");
-playGame("s");
-// playGame("i"); in testLetter
-playGame("k");
-
-
-
-// TEST
-// testLetter("s");
-// testLetter("o");
-// testLetter("e");
-// testLetter("t");
-
-// function testLetter(testLetter) {
-//     if (whatWord.guessLetter(testLetter)) {
-//         console.log("Letter " + testLetter + " was found");
-//         console.log(whatWord.displayWord());
-//     } else {
-//         console.log("Letter " + testLetter + " was NOT found");
-//         console.log(whatWord.displayWord());
-//     }
-// }
+// output to screen
+function outputStatus(hdr, msg) {
+    console.log("************* " + hdr + " *************");
+    console.log("\nLetter " + guessedLetter + " " + msg + ": " + gameWord.displayWord());
+};
